b9c2a07834676ee0779f1a13a0d448bb051d5fa8
MOJAN3543 <sjs732@naver.com>
2026-04-09 04:49:39 +0900

    Chore: prod, dev별 project name 분리

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,19 +5,23 @@ TARGET_ENV="${1:-dev}"
 
 if [ "$TARGET_ENV" = "prod" ]; then
   COMPOSE_FILE="docker-compose.prod.yml"
+  PROJECT_NAME="landom-prod"
 else
   COMPOSE_FILE="docker-compose.dev.yml"
+  PROJECT_NAME="landom-dev"
 fi
 
 ./scripts/env.sh "$TARGET_ENV"
 
 docker compose \
+  -p "$PROJECT_NAME" \
   --env-file .env.runtime \
   -f docker-compose.yml \
   -f "$COMPOSE_FILE" \
   pull
 
 docker compose \
+  -p "$PROJECT_NAME" \
   --env-file .env.runtime \
   -f docker-compose.yml \
   -f "$COMPOSE_FILE" \
